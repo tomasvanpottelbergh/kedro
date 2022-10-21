ded0d93a61bef318f23c223e62fbe75ca4adf454
Nok Chan <nok.lam.chan@quantumblack.com>
2022-10-21 16:12:14 +0100

    temporarily remove ShelveStore
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- kedro/framework/session/store.py
+++ kedro/framework/session/store.py
@@ -43,33 +43,33 @@ class BaseSessionStore(UserDict):
         )
 
 
-class ShelveStore(BaseSessionStore):
-    """Stores the session data on disk using `shelve` package."""
+# class ShelveStore(BaseSessionStore):
+#     """Stores the session data on disk using `shelve` package."""
 
-    _lock = Lock()
+#     _lock = Lock()
 
-    @property
-    def _location(self) -> Path:
-        return Path(self._path).expanduser().resolve() / self._session_id / "store"
+#     @property
+#     def _location(self) -> Path:
+#         return Path(self._path).expanduser().resolve() / self._session_id / "store"
 
-    def read(self) -> Dict[str, Any]:
-        """Read the data from disk using `shelve` package."""
-        data = {}  # type: Dict[str, Any]
-        try:
-            with shelve.open(str(self._location), flag="r") as _sh:  # nosec
-                data = dict(_sh)
-        except dbm.error:
-            pass
-        return data
+#     def read(self) -> Dict[str, Any]:
+#         """Read the data from disk using `shelve` package."""
+#         data = {}  # type: Dict[str, Any]
+#         try:
+#             with shelve.open(str(self._location), flag="r") as _sh:  # nosec
+#                 data = dict(_sh)
+#         except dbm.error:
+#             pass
+#         return data
 
-    def save(self) -> None:
-        """Save the data on disk using `shelve` package."""
-        location = self._location
-        location.parent.mkdir(parents=True, exist_ok=True)
+#     def save(self) -> None:
+#         """Save the data on disk using `shelve` package."""
+#         location = self._location
+#         location.parent.mkdir(parents=True, exist_ok=True)
 
-        with self._lock, shelve.open(str(location)) as _sh:  # nosec
-            keys_to_del = _sh.keys() - self.data.keys()
-            for key in keys_to_del:
-                del _sh[key]
+#         with self._lock, shelve.open(str(location)) as _sh:  # nosec
+#             keys_to_del = _sh.keys() - self.data.keys()
+#             for key in keys_to_del:
+#                 del _sh[key]
 
-            _sh.update(self.data)
+#             _sh.update(self.data)
